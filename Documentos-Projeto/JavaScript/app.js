220c8c31ced5df82fc42bdc9c8e19d6422f4aa81
Guilherme Freire Pires <guiga_freire06@hotmail.com>
2026-05-11 18:43:55 -0300

    Adicionando Funcionalidades aos botões

--- Documentos-Projeto/JavaScript/app.js
+++ Documentos-Projeto/JavaScript/app.js
@@ -1,7 +1,4 @@
-function addUsuario(){
-    let usuario = document.getElementById("usuario");
-    console.log(usuario.value)
-}
+let listaCadastrados = [];
 
 function textoTela(id, texto){
     let campo = document.getElementById(id);
@@ -17,18 +14,58 @@ function clicarBotao(variavel, id, texto){
 const botaoLogin = document.getElementById("btn-login");
 const botaoCadastro = document.getElementById("btn-cadastro");
 const botaoConfirmar = document.getElementById("btn-confirmar");
-const formulario = document.getElementById("form-login");
+const botaoConfirmarCadastro = document.getElementById("btn-confirmar-Cadastro")
+const formularioLogin = document.getElementById("form-login");
+const formularioCadastro = document.getElementById("form-cadastro")
 
-clicarBotao(botaoLogin, "sub-titulo", "Login");
 clicarBotao(botaoCadastro, "sub-titulo", "Cadastro");
 
-const confirmarBotao = botaoConfirmar.addEventListener("click", ()=>{
+const clicarLogin = botaoLogin.addEventListener("click", () =>{
+    textoTela("sub-titulo", "Faça seu Login:")
+    botaoLogin.style.display = "none";
+    botaoCadastro.style.display = "block";
+    formularioLogin.style.display = "block";
+    formularioCadastro.style.display ="none"
+})
+
+const clicarCadastro = botaoCadastro.addEventListener("click", () =>{
+    textoTela("sub-titulo", "Faça seu Cadastro:")
+    botaoCadastro.style.display = "none";
+    botaoLogin.style.display = "block";
+    formularioCadastro.style.display = "block";
+    formularioLogin.style.display = "none";
+})
+
+
+const confirmarLogin = botaoConfirmar.addEventListener("click", ()=>{
+    let usuario = document.getElementById("usuario")
+
     if(usuario.value == ''){
         alert('Informe algum nome!');
         return;
     }else{
-        addUsuario()
         textoTela("h1", `Bem vindo, ${usuario.value}!`);
-        textoTela("sub-titulo", "Cadastro Realizado com sucesso!");
+        textoTela("sub-titulo", "Login Realizado com sucesso!");
+        formularioLogin.style.display = "none";
+    }
+})
+
+const confirmarCadastro = botaoConfirmarCadastro.addEventListener("click", ()=>{
+    let AddUsuario = document.getElementById("AddUsuario")
+    let AddSenha = document.getElementById("AddSenha")
+    let AddSenha2 = document.getElementById("AddSenha-2")
+
+    if(AddSenha.value != AddSenha2.value){
+        alert("As senhas precisam ser iguais!");
+        return
+    }
+
+    if(AddUsuario.value == "" || AddSenha.value == "" || AddSenha2.value == ''){
+        alert('Preencha o campo de usuário!');
+        return;
+    }else{
+        textoTela("h1", `Bem vindo, ${AddUsuario.value}!`);
+        textoTela("sub-titulo", "Cadastro realizado com sucesso!");
+        formularioCadastro.style.display = "none";
     }
 })
